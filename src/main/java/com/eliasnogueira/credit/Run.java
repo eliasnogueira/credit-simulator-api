2974c58b1185cebdbf3afb1b039dbf987f71313b
eliasnogueira <elias.nogueira@gmail.com>
2020-10-18 11:58:55 +0200

    lombok removal

--- src/main/java/com/eliasnogueira/credit/Run.java
+++ src/main/java/com/eliasnogueira/credit/Run.java
@@ -24,18 +24,20 @@
 
 package com.eliasnogueira.credit;
 
-import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@Log4j2
-public class CreditSimulatorApplication {
+public class Run {
+
+    private static final Logger log = LogManager.getLogger(Run.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(CreditSimulatorApplication.class, args);
+        SpringApplication.run(Run.class, args);
         log.info("Application has started! Happy tests!");
     }
 }
